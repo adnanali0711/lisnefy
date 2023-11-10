0ab4701bb3ca84460f4c5d57b849471e85014194
MOHD ADNAN <105607385+adnanali0711@users.noreply.github.com>
2023-11-10 21:59:19 +0530

    new branch

--- src/listenify/Album.java
+++ src/listenify/Album.java
@@ -1,75 +1,81 @@
 package listenify;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Album {
-
     public String albumName;
 
     public String artistName;
 
     public List<Song> songList;
 
+    //Initialize the songList
     public Album(String albumName, String artistName) {
         this.albumName = albumName;
         this.artistName = artistName;
         this.songList = new ArrayList<>();
     }
 
-    boolean findSongInAlbum(String title){
+    public boolean findSongInAlbum(String title){
+
+        //Iterate over the arrayList and match
+        //each Songs title with title
         for(Song song:songList){
-            if((song.title).compareTo(title)==0) return true;
+
+            if((song.title).equals(title)){
+                return true;
+            }
         }
         return false;
     }
 
-    String addSongToAlbum(String title,double duration){
-        // first check if song is already present in the playlist or not
-        if(findSongInAlbum(title)){
+    public String addSongToAlbum(String title,double duration){
+        //Check if the song is already present we will not add
+        //otherwise we will create a new Song and add it to the songList;
+
+        if(findSongInAlbum(title)==true){
             return "Song is already present";
+        }else{
+            //I need to create a Song Object and then add it to songList
+            Song newSong = new Song(title,duration);
+            songList.add(newSong);
+            return "New Song has been added sucessfully";
         }
-        Song newSong = new Song(title,duration);
-        songList.add(newSong);
-        return "Song is successfully added";
     }
 
-    public String addSongToPlayList(int trackNumber, LinkedList<Song> songList){
-        int index = trackNumber-1;
+    public String addSongToPlayList(int trackNo, LinkedList<Song> playList){
+
+        //TrackNo is a no in the songList
+
+        //TrackNo : 1,2,3,4,5......
+        //indices : 0,1,2,3,4.....
+        int index = trackNo -1;
+
+        //Checking for validity of index
         if(index>=0 && index<this.songList.size()){
+
             Song song = this.songList.get(index);
-            songList.add(song);
-            return "Your song sucessfully added";
+            playList.add(song);
 
+            return "Song added to the playList";
         }
-        return "Your song is not added because of invalid track number";
+        return "Invalid track No";
     }
 
-    public String addSOngToPlayList(String title,LinkedList playList){
-        if(findSongInAlbum(title)){
-            for(Song song:this.songList){
-                if(song.title.equals(title)){
-                    playList.add(song);
-                    return "Song is sucessfully addes";
-                }
-            }
+    public String addSongToPlayList(String title,LinkedList<Song> playList){
 
-        }
-    return "incorrect title";
-    }
+        //I need to find out that Song with that title
+        //and put it in the playlist
 
-    public void play(){
+        //Iterate over the songList:
         for(Song song : songList){
-            System.out.println(toString(song));
+
+            if(song.title==title){
+                playList.add(song);
+                return "Song has been added successfully";
+            }
         }
-        return;
-    }
-    public String toString(Song song) {
-        return "Song{" +
-                "title='" + song.title + '\'' +
-                ", duration=" + song.duration +
-                '}';
+        return "Song does not exist";
     }
 
 }
